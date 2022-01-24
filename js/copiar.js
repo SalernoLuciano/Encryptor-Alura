c291e84c19954222393810d79581d82ce92e11fe
Luciano Salerno <salerno.luciano@outlook.com.ar>
2022-01-24 10:18:53 -0300

    Revert "TextArea deshabilitada para escribir"
    This reverts commit 15776f28c1e20178fd8cc3323667505f2e636b1f.

--- js/copiar.js
+++ js/copiar.js
@@ -2,8 +2,6 @@ var btnCopy = document.querySelector("#btn-copy");
 var textArea = document.querySelector("#result");
 
 btnCopy.addEventListener( "click", () => {
-    textArea.disabled = false;
     textArea.select();
     document.execCommand("copy");
-    textArea.disabled = true;
 });
