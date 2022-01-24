15776f28c1e20178fd8cc3323667505f2e636b1f
Luciano Salerno <salerno.luciano@outlook.com.ar>
2022-01-24 10:09:56 -0300

    TextArea deshabilitada para escribir

--- js/copiar.js
+++ js/copiar.js
@@ -2,6 +2,8 @@ var btnCopy = document.querySelector("#btn-copy");
 var textArea = document.querySelector("#result");
 
 btnCopy.addEventListener( "click", () => {
+    textArea.disabled = false;
     textArea.select();
     document.execCommand("copy");
+    textArea.disabled = true;
 });
